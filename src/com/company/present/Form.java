13240b2d486d5aa22e04630d2935ba1f5d76b225
tvblackman1 <vbif.misha2012@yandex.ru>
2021-01-06 23:21:56 +0300

    java, fuck u

--- src/com/company/present/Form.java
+++ src/com/company/present/Form.java
@@ -2,61 +2,76 @@ package com.company.present;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Form extends JFrame implements Runnable {
+
+
+    private final int w = 700;
+    private final int h = 600;
+
+    private BufferedImage buf = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+    private BufferedImage img = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+    private BufferedImage graph = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+
+    private int frame = 0;
     int i = 0;
+    private final int FRAMES_TOTAL = 100000;
+
     final int DELAY = 25;
     public Form() {
         super("Natural Selection");
-        setSize(700, 600);
+        setSize(w, h);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
 
     @Override
     public void paint(Graphics g) {
-        super.paint(g);
-        Graphics2D gr2d = (Graphics2D) g;
-//        gr2d.setBackground(Color.LIGHT_GRAY);
-        gr2d.setBackground(Color.RED);
-        gr2d.setPaint(Color.BLACK);
-        gr2d.drawLine(100 + i++, 100, 200, 200);
+//        super.paint(g);
+
+        Graphics2D graphics2D = (Graphics2D) g;
+        g.clearRect(0, 0, w, h);
+        ((Graphics2D) g).setBackground(Color.LIGHT_GRAY);
+
+        graphics2D.setPaint(Color.BLACK);
+        graphics2D.drawLine(100 + (i++)/100, 100, 200, 200);
     }
 
     @Override
     public void run() {
-//        while(true) {
-//            this.repaint();
-////            this.paintComponents();
-//        }
-
-        long beforeTime, timeDiff, sleep;
-
-        beforeTime = System.currentTimeMillis();
-
-        while (true) {
-            repaint();
-
-            timeDiff = System.currentTimeMillis() - beforeTime;
-            sleep = DELAY - timeDiff;
-
-            if (sleep < 0) {
-                sleep = 2;
-            }
-
-            sleep = 50;
-
-            try {
-                Thread.sleep(sleep);
-            } catch (InterruptedException e) {
-
-                String msg = String.format("Thread interrupted: %s", e.getMessage());
-
-                JOptionPane.showMessageDialog(this, msg, "Error",
-                        JOptionPane.ERROR_MESSAGE);
-            }
-
-            beforeTime = System.currentTimeMillis();
+        while(true) {
+            this.repaint();
         }
+
+//        long beforeTime, timeDiff, sleep;
+//
+//        beforeTime = System.currentTimeMillis();
+//
+//        while (frame < FRAMES_TOTAL) {
+//            repaint();
+////
+////            timeDiff = System.currentTimeMillis() - beforeTime;
+////            sleep = DELAY - timeDiff;
+////
+////            if (sleep < 0) {
+////                sleep = 2;
+////            }
+////
+////            sleep = 50;
+////
+////            try {
+////                Thread.sleep(sleep);
+////            } catch (InterruptedException e) {
+////
+////                String msg = String.format("Thread interrupted: %s", e.getMessage());
+////
+////                JOptionPane.showMessageDialog(this, msg, "Error",
+////                        JOptionPane.ERROR_MESSAGE);
+////            }
+////
+////            beforeTime = System.currentTimeMillis();
+//        }
     }
 }
